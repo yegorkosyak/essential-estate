fc0b741923e6e3ff01cee444ab80acf8fa0e4f77
Yehor Kosiak <yegorkosyak32@gmail.com>
2023-05-17 19:15:00 +0200

    feat: polish as initial local

--- src/i18n.js
+++ src/i18n.js
@@ -9,7 +9,7 @@ i18n
   // .use(I18nextBrowserLanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    fallbackLng: "en",
+    fallbackLng: "pl",
     // debug: true,
 
     interpolation: {
