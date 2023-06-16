3d70f681f54b14fd81e0ca0716d4753ca23ba67d
Yehor Kosiak <yegorkosyak32@gmail.com>
2023-06-16 15:24:47 +0200

    feat: fetch agents from strapi api

--- src/components/agents/Agents.jsx
+++ src/components/agents/Agents.jsx
@@ -1,14 +1,11 @@
 import styled from "styled-components";
-import { useEffect, createRef } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import { SectionTitle } from "../../styles/components/Title";
 
 import { theme } from "../../styles/utility/global-theme.mjs";
 import { device } from "../../styles/utility/media-breakpoints.mjs";
-import Andrew from "../../assets/images/agents/andrew.png";
-import Eli from "../../assets/images/agents/eli.png";
-import Polina from "../../assets/images/agents/polina.png";
-import Viacheslav from "../../assets/images/agents/viacheslav.png";
 
 import { useTranslation } from "react-i18next";
 
@@ -30,40 +27,33 @@ const Agent = ({ image, agentName, comment, quote }) => {
   );
 };
 
-export default function Agents() {
+export default function Agents({ locale }) {
   const { t } = useTranslation();
+  const [agents, setAgents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:1337/api/agents?locale=${locale}&populate=*`)
+      .then(({ data }) => {
+        setAgents(data.data);
+      });
+  }, [locale]);
   return (
     <AgentsContainer id="agents">
       <SectionTitle color={theme.brandWhite}>{t(`Agents.Title`)}</SectionTitle>
 
       <AgentsWrap>
-        <Agent
-          image={Andrew}
-          agentName="Andrew"
-          comment={t(`Agents.Andrew`)}
-          quote={t("Agents.AndrewQuote")}
-        />
-
-        <Agent
-          image={Eli}
-          agentName="Elizaveta"
-          comment={t(`Agents.Elizaveta`)}
-          quote={t("Agents.ElizavetaQuote")}
-        />
-
-        <Agent
-          image={Polina}
-          agentName="Polina"
-          comment={t(`Agents.Polina`)}
-          quote={t("Agents.PolinaQuote")}
-        />
-
-        <Agent
-          image={Viacheslav}
-          agentName="Viacheslav"
-          comment={t(`Agents.Viacheslav`)}
-          quote={t("Agents.ViacheslavQuote")}
-        />
+        {agents.map(({ id, attributes }) => (
+          <Agent
+            key={id}
+            image={
+              "http://localhost:1337" + attributes.photo.data.attributes.url
+            }
+            agentName={attributes.name}
+            comment={attributes.description}
+            quote={attributes.quote}
+          />
+        ))}
       </AgentsWrap>
     </AgentsContainer>
   );
@@ -81,7 +71,6 @@ const AgentsWrap = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
   grid-gap: 3rem;
   @media ${device.tablet} {
     grid-template-columns: unset;
@@ -125,6 +114,7 @@ const AgentName = styled.span`
   color: ${(props) => props.theme.brandWhite};
   font-weight: ${(props) => props.theme.weightBlack};
   font-size: 1.7rem;
+  margin-right: 5px;
 `;
 
 const AgentComment = styled.span`
