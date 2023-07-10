a0bb78aa6021b53cb1d97f0e6d871ddc4dcefda2
Yehor Kosiak <yegorkosyak32@gmail.com>
2023-07-10 19:41:29 +0200

    fix: point to api url

--- src/utils/apiUrl.jsx
+++ src/utils/apiUrl.jsx
@@ -1,4 +1,4 @@
 export const apiUrl =
-  process.env.NODE_ENV === "prod"
-    ? process.env.REACT_APP_API_URL_PROD
-    : process.env.REACT_APP_API_URL_DEV;
+  process.env.NODE_ENV === "dev"
+    ? process.env.REACT_APP_API_URL_DEV
+    : process.env.REACT_APP_API_URL_PROD;
